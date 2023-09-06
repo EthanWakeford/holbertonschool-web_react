d61755b4e8f43dd8ed47502343cb376628d2b15a
EthanWakeford <ethanwakeford@gmail.com>
2023-09-06 16:00:46 -0500

    task 0

---
+++ react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <header className='App-header'>
+        <div className='App-logo'></div>
+        <h1>School dashboard</h1>
+      </header>
+      <main className='App-body'>
+        <p>Login to access the full dashboard</p>
+      </main>
+      <footer className='App-footer'>
+        <p>
+          <i>Copyright 2020 - holberton School</i>
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
