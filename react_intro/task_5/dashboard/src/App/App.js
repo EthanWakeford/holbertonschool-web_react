8ef2691bc83bd993bbe14cadf28932505bb2f16a
EthanWakeford <ethanwakeford@gmail.com>
2023-09-07 14:17:59 -0500

    task 10

--- react_intro/task_5/dashboard/src/App/App.js
+++ react_intro/task_5/dashboard/src/App/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import { getFullYear, getFooterCopy } from './utils';
+import { getFullYear, getFooterCopy } from '../Utils/utils';
 
 function App() {
   return (
