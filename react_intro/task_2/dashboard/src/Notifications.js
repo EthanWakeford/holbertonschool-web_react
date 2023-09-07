b09c496d9d2d5116c7cbf8381d3d5af5d12e9c16
EthanWakeford <ethanwakeford@gmail.com>
2023-09-07 12:05:51 -0500

    task 3

--- react_intro/task_2/dashboard/src/Notifications.js
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import './Notifications.css';
+import { getLatestNotification } from './utils';
 
 export default function Notifications() {
   return (
     <div className='Notifications'>
       <p>Here is the list of notifications</p>
+      <button
+        style={{ float: 'right' }}
+        aria-label='close'
+        onClick={() => console.log('Close button has been clicked')}
+      >
+        <img src='./holbie.ico' alt='idk'></img>
+      </button>
+      <ul>
+        <li data-priority='default'>New course available</li>
+        <li data-priority='urgent'>New resume available</li>
+        <li
+          data-priority='urgent'
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
     </div>
   );
 }
