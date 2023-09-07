7137db2082dc0e0a7d25c8406b0090cb25bb54e1
EthanWakeford <ethanwakeford@gmail.com>
2023-09-07 14:13:11 -0500

    task 10

---
+++ react_intro/task_5/dashboard/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <>
+      <header className='App-header'>
+        <div className='App-logo'></div>
+        <h1>School dashboard</h1>
+      </header>
+      <main className='App-body'>
+        <p>Login to access the full dashboard</p>
+        <label htmlFor='email'>Email</label>
+        <input type='email' name='email'></input>
+        <label htmlFor='password'>Password</label>
+        <input type='password' name='password'></input>
+        <button>OK</button>
+      </main>
+      <footer className='App-footer'>
+        <p>
+          <i>
+            Copyright {getFullYear()} - {getFooterCopy(true)}
+          </i>
+        </p>
+      </footer>
+    </>
+  );
+}
+
+export default App;
--- react_intro/task_5/dashboard/src/index.js
+++ react_intro/task_5/dashboard/src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Notifications from './Notifications';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <div className='root-notifications'>
+      <Notifications />
+    </div>
+    <App />
+  </React.StrictMode>
+);
