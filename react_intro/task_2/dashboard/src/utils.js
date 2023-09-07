bbf5ea48e0101b4a51d8d1c210e33a6539e8e267
EthanWakeford <ethanwakeford@gmail.com>
2023-09-07 11:38:13 -0500

    task 2

---
+++ react_intro/task_2/dashboard/src/utils.js
@@ -0,0 +1,9 @@
+// assorted functions
+
+export function getFullYear() {
+  return new Date().getFullYear();
+}
+
+export function getFooterCopy(isIndex) {
+  return isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+}
