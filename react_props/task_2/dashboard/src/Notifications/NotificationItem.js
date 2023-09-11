c08f6d1120183994ecd950df00ea2fb7d3712cf3
EthanWakeford <ethanwakeford@gmail.com>
2023-09-11 12:08:50 -0500

    task 2

---
+++ react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -0,0 +1,9 @@
+export default function NotificationItem({ type, html, value }) {
+  return (
+    <>
+      <li data-notification-type={type} dangerouslySetInnerHTML={html}>
+        {value}
+      </li>
+    </>
+  );
+}
