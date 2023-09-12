35068898050f34cd0708d7fc48f71a7abd9e3ceb
EthanWakeford <ethanwakeford@gmail.com>
2023-09-12 15:51:21 -0500

    tests

--- React_component/task_0/dashboard/src/Utils/utils.test.js
+++ React_component/task_0/dashboard/src/Utils/utils.test.js
@@ -1,9 +1,12 @@
 // utils testing
 import { getFooterCopy, getFullYear, getLatestNotification } from './utils';
+import 'jest';
 
-expect(getFullYear()).toBe(2023);
-expect(getFooterCopy(true)).toBe('Holberton School');
-expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
-expect(getLatestNotification()).toBe(
-  '<strong>Urgent requirement</strong> - complete by EOD'
-);
+test('test', () => {
+  expect(getFullYear()).toBe(2023);
+  expect(getFooterCopy(true)).toBe('Holberton School');
+  expect(getFooterCopy(false)).toBe('Holberton School main dashboard');
+  expect(getLatestNotification()).toBe(
+    '<strong>Urgent requirement</strong> - complete by EOD'
+  );
+});
