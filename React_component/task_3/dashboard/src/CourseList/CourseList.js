27f703241a04d231b1c333d6eb872ef95292f944
EthanWakeford <ethanwakeford@gmail.com>
2023-09-12 21:55:14 -0500

    task 3

---
+++ React_component/task_3/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,40 @@
+import CourseListRow from './CourseListRow';
+import './CourseList.css';
+import CourseShape from './CourseShape';
+import PropTypes from 'prop-types';
+
+export default function CourseList({ listCourses }) {
+  return (
+    <table id='CourseList'>
+      <thead>
+        <CourseListRow textFirstCell={'Available courses'} isHeader={true} />
+        <CourseListRow
+          textFirstCell={'Course name'}
+          textSecondCell={'Credit'}
+          isHeader={true}
+        />
+      </thead>
+      <tbody>
+        {listCourses ? (
+          listCourses.map((course) => (
+            <CourseListRow
+              key={course.id}
+              textFirstCell={course.name}
+              textSecondCell={course.number}
+            />
+          ))
+        ) : (
+          <tr>No course available yet</tr>
+        )}
+      </tbody>
+    </table>
+  );
+}
+
+CourseList.propTypes = {
+  listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultTypes = {
+  listCourses: [],
+};
