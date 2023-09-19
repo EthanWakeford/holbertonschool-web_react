e28bc96f5fd268cc5cdbaf67e66f0926256f9626
EthanWakeford <ethanwakeford@gmail.com>
2023-09-19 12:23:27 -0500

    task 2

--- react_state/task_2/dashboard/src/Header/Header.js
+++ react_state/task_2/dashboard/src/Header/Header.js
@@ -1,4 +1,6 @@
 import { StyleSheet, css } from 'aphrodite';
+import { useContext } from 'react';
+import AppContext from '../App/AppContext';
 
 const styles = StyleSheet.create({
   logo: {
@@ -28,8 +30,16 @@ const styles = StyleSheet.create({
 });
 
 export default function Header() {
+  const { user, logOut } = useContext(AppContext);
+
   return (
     <header className={css(styles.header)}>
+      {user.isLoggedIn && (
+        <div>
+          <p>Welcome {user.email}</p>
+          <button onClick={logOut}>log Out</button>
+        </div>
+      )}
       <div className={css(styles.logo)}></div>
       <h1 className={css(styles.headerHeading)}>School dashboard</h1>
     </header>
