301ab5ad18919d7a73a3bda6be024a690d0a5c6d
EthanWakeford <ethanwakeford@gmail.com>
2023-09-26 10:26:05 -0500

    added task 1

--- 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
+++ 0x08_react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.js
@@ -1,9 +1,27 @@
 import * as notifications from '../../../../notifications.json';
+import { normalize, schema } from 'normalizr';
 
-function getAllNotificationsByUser(userId) {
-  return notifications
-    .filter((notif) => notif.author.id === userId)
-    .map((notif) => notif.context);
+function normalizeData() {
+  const user = new schema.Entity('users');
+  const message = new schema.Entity(
+    'messages',
+    {},
+    {
+      idAttribute: 'guid',
+    }
+  );
+  const notification = new schema.Entity('notifications', {
+    author: user,
+    context: message,
+  });
+  return normalize(notifications, [notification]);
 }
 
+function getAllNotificationsByUser(userId) {
+  const normalizedData = normalizeData();
+
+  return normalizedData.entities.users[userId];
+}
+
+export { normalizeData };
 export default getAllNotificationsByUser;
